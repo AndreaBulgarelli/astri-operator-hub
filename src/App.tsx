3af8d2093f4e832edfee74b01d932888c445e421
burnout87 <gabriele.barni@gmail.com>
2025-12-03 14:54:00 +0100

    added future flags for React v7

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,10 @@ const App = () => (
         <Toaster />
         <Sonner />
         <RightPanelProvider>
-          <BrowserRouter>
+          <BrowserRouter future={{
+              v7_startTransition: true,
+              v7_relativeSplatPath: true
+            }}>
             <Routes>
               <Route path="/" element={<Index />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
